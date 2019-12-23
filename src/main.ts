65a4af13b6d459868582833a9a959095cf52e7a8
anthony sebastian <antsebastian.as@gmail.com>
2019-12-22 17:28:51 -0800

    added swipe to carousel

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -6,6 +7,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
