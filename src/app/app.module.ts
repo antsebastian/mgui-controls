1e2403b8c469e8374d8bcfd8eff806ddbd1dbf2a
anthony sebastian <antsebastian.as@gmail.com>
2019-12-23 14:07:13 -0800

    fixed prod compile error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { CacheReuseStrategy } from './route-cache';
 import { HammerGestureConfig } from "@angular/platform-browser";
 import * as hammer from "hammerjs";
  
-class MyHammerConfig extends HammerGestureConfig {
+export class MyHammerConfig extends HammerGestureConfig {
   overrides = <any>{
     swipe: { direction: hammer.DIRECTION_HORIZONTAL },
     pinch: { enable: false },
